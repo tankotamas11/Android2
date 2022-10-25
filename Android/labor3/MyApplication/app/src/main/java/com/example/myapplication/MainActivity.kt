ee73351c20b0eac3d6569c2003a881547ab3ff45
tankotamas11 <tankotamaszene@gmail.com>
2022-10-26 02:14:29 +0300

    kis modositasok

--- Android/labor3/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Android/labor3/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -10,30 +10,41 @@ import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.example.myapplication.fragments.MenuFragment
 
 class MainActivity : AppCompatActivity() {
     val TAG= "MainActivity"
 
-    val choosephonenum=1
-    lateinit var personName : TextView
-    lateinit var startButton : Button
-    lateinit var choosebtn : Button
+//    val choosephonenum=1
+//    lateinit var personName : TextView
+//    lateinit var startButton : Button
+//    lateinit var choosebtn : Button
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.i(TAG,"onCreate() called.")
-        startButton=findViewById(R.id.startbutton)
-        startButton.setOnClickListener { callActivity() }
 
-        personName=findViewById(R.id.PersonName)
-        choosebtn=findViewById(R.id.choose)
-        choosebtn.setOnClickListener {
-        val intent= Intent(Intent.ACTION_PICK,ContactsContract.CommonDataKinds.Phone.CONTENT_URI)
-        startActivity(intent)
+        val homeFragment = MenuFragment()
+        val fragment : Fragment? =
+            supportFragmentManager.findFragmentByTag(MenuFragment::class.java.simpleName)
+
+        if (fragment !is MenuFragment){
+            supportFragmentManager.beginTransaction().add(R.id.container_fragment, homeFragment,MenuFragment::class.java.simpleName).commit()
+        }
+
+//        Log.i(TAG,"onCreate() called.")
+//        startButton=findViewById(R.id.startbutton)
+//        startButton.setOnClickListener { callActivity() }
+//
+//        personName=findViewById(R.id.PersonName)
+//        choosebtn=findViewById(R.id.choose)
+//        choosebtn.setOnClickListener {
+//        val intent= Intent(Intent.ACTION_PICK,ContactsContract.CommonDataKinds.Phone.CONTENT_URI)
+//        startActivity(intent) }
 
 
-       }
+
 
 
     }
--- Android/labor3/MyApplication/app/src/main/java/com/example/myapplication/fragments/MenuFragment.kt
+++ Android/labor3/MyApplication/app/src/main/java/com/example/myapplication/fragments/MenuFragment.kt
@@ -1,63 +1,64 @@
 package com.example.myapplication.fragments
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 import com.example.myapplication.R
+import com.example.myapplication.databinding.ActivityMainBinding
+import com.example.myapplication.databinding.FragmentMenuBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Menu.newInstance] factory method to
- * create an instance of this fragment.
- */
-class Menu : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-    private lateinit var startbttn : Button
+class MenuFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu, container, false)
-    }
+        val bind= FragmentMenuBinding.inflate(layoutInflater)
+        val nexfragment = QuestionFragment()
+        bind.startbutton.setOnClickListener {
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        view.apply {
-            initViewItems(this)
-            registerListeners()
+
+            fragmentManager?.beginTransaction()?.apply {
+                replace(R.id.container_fragment,nexfragment, QuestionFragment::class.java.simpleName)
+                    .addToBackStack(null)
+                    .commit()
+            }
         }
+
+        return bind.root
     }
 
-    private fun initViewItems(view: View) {
-        startbttn=view.findViewById(R.id.startbutton)
-    }
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//        view.apply {
+//            textView = view.findViewById(R.id.textView)
+//            startbttn=view.findViewById(R.id.startbutton)
+//            startbttn.setOnClickListener {
+//                findNavController().navigate(R.id.action_menu3_to_question3)
+//            }
+//        }
+//   }
 
-    private fun registerListeners() {
-        startbttn.setOnClickListener {
-            findNavController().navigate(R.id.action_menu3_to_question3)
-        }
-    }
+//    private fun initViewItems(view: View) {
+//        startbttn=view.findViewById(R.id.startbutton)
+//    }
+//
+//    private fun registerListeners() {
+//        startbttn.setOnClickListener {
+//            findNavController().navigate(R.id.action_menu3_to_question3)
+//        }
+//    }
 
 
 }
