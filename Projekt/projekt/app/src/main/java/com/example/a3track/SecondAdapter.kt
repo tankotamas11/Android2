60eb2ccf51d922014d0c6cb80339033160c863aa
tankotamas11 <tankotamaszene@gmail.com>
2023-01-01 21:32:27 +0200

    tasks and second adapter

--- Projekt/projekt/app/src/main/java/com/example/a3track/SecondAdapter.kt
+++ Projekt/projekt/app/src/main/java/com/example/a3track/SecondAdapter.kt
@@ -1,31 +1,97 @@
 package com.example.a3track
 
+import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.a3track.model.Task
+import com.example.a3track.model.TasksResponse
+import com.example.a3track.model.User
+import com.example.a3track.viewmodels.UserViewModel
 import java.text.FieldPosition
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
-class SecondAdapter(private val TaskList: ArrayList<Task>):RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
-    private  lateinit var  mListener : MyAdapter.onItemClickListener
-    interface  OnItemClickListener{
-        fun onItemClick(position: Int)
-    }
-    fun setOnItemClickListener(listener: MyAdapter.onItemClickListener){
-        mListener=listener
-    }
+class SecondAdapter(private val TaskList: ArrayList<TasksResponse>,val users: List<User>):RecyclerView.Adapter<SecondAdapter.SecondViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyAdapter.MyViewHolder {
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SecondAdapter.SecondViewHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.taskitem,parent,false)
-        return MyAdapter.MyViewHolder(itemView)
+        return SecondViewHolder(itemView)
+    }
+    fun Name( id:Int):String{
+
+        var t=true
+        var i=0
+        while (t){
+            if(users!![i].ID==id){
+                return users!![i].last_name+ " "+ users!![i].first_name
+            }
+        }
+        return "ID problem"
+    }
+    fun convertLongToTime(time: Long): String {
+        val date = Date(time)
+        val format = SimpleDateFormat("yyyy.MM.dd HH:mm")
+        return format.format(date)
+    }
+    fun Priority(level:Int):String{
+        when (level){
+            1 -> return "High prio"
+            2 -> return  "Medium prio"
+            else -> {
+                return "Low prio"
+            }
+        }
+    }
+    fun TaskStatus(level:Int):String{
+        when (level){
+            1 -> return "New"
+            2 -> return  "In progress"
+            3 -> return  "Done"
+            else -> {
+                return "Blocked"
+            }
+        }
     }
 
-    override fun onBindViewHolder(holder: MyAdapter.MyViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: SecondAdapter.SecondViewHolder, position: Int) {
+
+
+        val currentItem=TaskList[position]
+
+
+        holder.title.text=currentItem.title
+        holder.creator.text=currentItem.title//Name(currentItem.created_by_user_ID)+convertLongToTime(currentItem.created_time)
+        holder.assigned.text=Name(currentItem.asigned_to_user_ID)
+        holder.tdeadline.text=convertLongToTime(currentItem.deadline)
+        holder.priority.text=Priority(currentItem.priority)
+        holder.description.text=currentItem.description
+        holder.status.text=TaskStatus(currentItem.status)
 
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return  TaskList.size
+    }
+    class SecondViewHolder(itemview : View) :RecyclerView.ViewHolder(itemview){
+        val  title : TextView = itemview.findViewById(R.id.taskTitle)
+        val creator : TextView=itemview.findViewById(R.id.taskCreator)
+        val tdeadline : TextView=itemview.findViewById(R.id.taskdeadline)
+        val assigned : TextView=itemview.findViewById(R.id.taskassigned)
+        val priority : TextView=itemview.findViewById(R.id.taskpriority)
+        val status : TextView=itemview.findViewById(R.id.taskstatus)
+        val description : TextView=itemview.findViewById(R.id.taskdescription)
+
+
+
+
     }
 
 }
--- Projekt/projekt/app/src/main/java/com/example/a3track/fragments/TasksFragment.kt
+++ Projekt/projekt/app/src/main/java/com/example/a3track/fragments/TasksFragment.kt
@@ -11,12 +11,17 @@ import android.widget.TextView
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.get
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.a3track.MyAdapter
 import com.example.a3track.R
+import com.example.a3track.SecondAdapter
 import com.example.a3track.databinding.FragmentTasksBinding
+import com.example.a3track.model.Task
+import com.example.a3track.model.TasksResponse
+import com.example.a3track.model.User
 import com.example.a3track.repository.TrackerRepository
-import com.example.a3track.viewmodels.CurrentUserViewModel
-import com.example.a3track.viewmodels.TasksViewModel
-import com.example.a3track.viewmodels.TasksViewModelFactory
+import com.example.a3track.viewmodels.*
 
 
 class TasksFragment : Fragment() {
@@ -25,11 +30,16 @@ class TasksFragment : Fragment() {
     private lateinit var tasksVM:TasksViewModel
     private var _binding:FragmentTasksBinding?=null
     private val binding get() = _binding!!
+    private lateinit var adapter2: SecondAdapter
+    private lateinit var recyclerView2: RecyclerView
+    private lateinit var userListVM: UserViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val factory= TasksViewModelFactory(TrackerRepository())
         tasksVM=ViewModelProvider(this,factory).get(TasksViewModel::class.java)
+        val factory1= UserViewModelFactory(TrackerRepository())
+        userListVM=ViewModelProvider(this,factory1).get(UserViewModel::class.java)
     }
 
     override fun onCreateView(
@@ -51,12 +61,30 @@ class TasksFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val szoveg:TextView= view.findViewById(R.id.tasksTextView)
+        szoveg.text="My Tasks"
         val sharedPreferences = requireActivity().getSharedPreferences("TRACKER", Context.MODE_PRIVATE)
         val retrievedToken = sharedPreferences.getString("token",null)
+
         tasksVM.readTasks(retrievedToken.toString())
         tasksVM.taskList.observe(viewLifecycleOwner){Log.i("TTT","token1: " + retrievedToken.toString())
             val tasks=tasksVM.taskList.value
-            //szoveg.text=tasks!!.size.toString()
+            userListVM.readUsers(retrievedToken.toString())
+            userListVM.userList.observe(viewLifecycleOwner) {
+                var userList = userListVM.userList.value as ArrayList<User>
+
+
+                val layoutManager= LinearLayoutManager(context)
+                recyclerView2=view.findViewById(R.id.recyclerView2)
+
+                recyclerView2.layoutManager=layoutManager
+                recyclerView2.setHasFixedSize(true)
+                Log.i("TTT","userlist:"+tasks!![0].title)
+                val segedlista=  tasks as ArrayList<TasksResponse>
+                Log.i("TTT","userlist:"+segedlista[0].title)
+                adapter2= SecondAdapter(tasks as ArrayList<TasksResponse>,userList as ArrayList<User>)
+                //recyclerView2.adapter=adapter2
+            }
+
             Log.i("TTT","sikeres lekerdezes")
         }
 
